8829f4fc7916409249fd028ff668a723c50c436d
jadesolayo <oloyedeyetundejadesola@gmail.com>
2023-02-26 14:22:18 +0100

    added new code to script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -13,7 +13,9 @@ if (hasDriversLincense) console.log(`i can drive!`);
 
 // Functions
 // A fnction is a piece of code that can be reuses over and over again.
+
 // Functions allow us to write more maintainable codes(DRY)(Don't Repeat Yourself)
+
 // Functions are the fundamental building blocks of javaScript programs
 
 function logger() {
@@ -26,13 +28,13 @@ logger();
 
 // Parameters are variables that are specific to a function and will be defined when the function is called
 
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
-  return juice;
-}
-const fruitJuice = fruitProcessor(3, 6);
-console.log(fruitJuice);
-console.log(fruitProcessor(8, 7));
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
+// const fruitJuice = fruitProcessor(3, 6);
+// console.log(fruitJuice);
+// console.log(fruitProcessor(8, 7));
 
 // Function Declarations and Function Expression
 
@@ -51,3 +53,83 @@ const age2 = calcAge2(2000);
 console.log(age1, age2);
 
 // The major difference between a function expression and a function declaration is that function declaration can be initialized before declaraing while function expression cannot.
+
+// Arrow Functions
+
+const calcAge3 = (birthYear) => 2023 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2023 - birthYear;
+//   const retirement = 65 - age;
+//   // return retirement;
+//   return `${firstName} will retire in ${retirement} years`;
+// };
+// console.log(yearsUntilRetirement(1995, "Jadesola"));
+
+// Written with normal function
+
+/* 
+function yearsUntilRetirement2(birthYear, firstName) {
+  const age2 = 2023 - birthYear;
+  const retirement = 65 - age2;
+  return `${firstName} will retire in ${retirement} years`;
+}
+console.log(yearsUntilRetirement2(1990, "Esther")); 
+*/
+
+// FUNCTIONS CALLING OTHER FUNCTIONS
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces`;
+  return juice;
+}
+console.log(fruitProcessor(3, 6));
+
+// REVIEWING FUNCTIONS
+// the return statement immediately exit a function once it is added and everything after the return statement is ignored.
+
+const calcAge = function (birthYear) {
+  return 2023 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age2 = calcAge(birthYear);
+  const retirement = 65 - age2;
+
+  if (retirement > 0) {
+    console.log(`${firstName} will retire in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+};
+console.log(yearsUntilRetirement(1990, "Esther"));
+console.log(yearsUntilRetirement(1940, "Rebecca"));
+
+// The Three types of functions and how they are written:
+
+// Function Declaration
+
+// function calcAge(birthYear) {
+//   return 2023 - birthYear;
+// }
+
+// Function Expression
+
+// const calcAge = function (birthYear){
+//   return 2023 - birthYear;
+// }
+
+// Arrow Function
+
+// const calcAge = birthYear => 2037 - birthYear;
