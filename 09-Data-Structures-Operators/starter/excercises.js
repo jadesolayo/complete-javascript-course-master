2930bad364718dc858c854d7e452a508949782a8
jadesolayo <oloyedeyetundejadesola@gmail.com>
2023-05-11 04:09:00 +0100

    modified js

--- 09-Data-Structures-Operators/starter/excercises.js
+++ 09-Data-Structures-Operators/starter/excercises.js
@@ -1,1 +1,14 @@
 // Excercises to Test my knowledge
+'use strict';
+
+const fruits = ['apple', 'banana', 'orange'];
+// Using array destructuring
+const [fruit1, fruit2, fruit3] = fruits;
+console.log(fruit1, fruit2, fruit3);
+
+const getNumbers = function () {
+  return [1, 2, 3];
+};
+console.log(getNumbers());
+const [firstNum, secondNum, thirdNum] = getNumbers();
+console.log(firstNum, secondNum, thirdNum);
